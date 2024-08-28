72b94139ebf001dd3f74a30e7706dc45fb8fa20d
Randy_Dwk <randy.dewancker@gmail.com>
2024-08-28 13:51:47 +0200

    client

--- server/db.js
+++ server/db.js
@@ -1,10 +1,14 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false, // Optional, depending on your setup
-  },
+  user: process.env.PGUSER || process.env.POSTGRES_USER,
+  host: process.env.PGHOST || process.env.PGHOST,
+  database: process.env.PGDATABASE || process.env.POSTGRES_DB,
+  password: process.env.PGPASSWORD || process.env.POSTGRES_PASSWORD,
+  port: process.env.PGPORT || 5432,
+  ssl: process.env.NODE_ENV === 'production' ? {
+    rejectUnauthorized: false,
+  } : false,
 });
 
 module.exports = pool;
